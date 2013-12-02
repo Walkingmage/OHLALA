785ed6da3ac6de8408e4834740fabf747c6d78aa
Dennis Ahlqvist <dennisahlqvist@hotmail.com>
2013-12-02 12:25:53 +0100

    fixed footer in manage_user.php

--- manage_user.php
+++ manage_user.php
@@ -98,10 +98,10 @@
 
 
 </div>
-	
+
 <footer>
-  <p>Contact details to web support</p>
-  <p>Site map</p>
+  <p>support@jensenoffline.com</p>
+  <p>The footer will be moved down a bit with js. Not implemented in mockup</p>
 </footer>
 	
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
