71ffcbaaf09c074016fea71c986a44ef6c4c1c97
Dennis Ahlqvist <dennisahlqvist@hotmail.com>
2013-12-03 11:24:13 +0100

    lade till dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+﻿<?php
+session_start();
+define("INC", 1);
+//die("Används ej");
+include("config.mysqli.php");
+
+
+@$mysqli = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("No Db found");
+//echo($mysqli->character_set_name());
+//$mysqli->set_charset("utf8") or die("Can't set charset");
+//echo($mysqli->character_set_name());
+
+
+?>
