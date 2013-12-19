ba72f9093c80973bfe0da54fb042b8614f8017e1
jimmijohansson <jimmi.johansson1@gmail.com>
2013-12-19 22:22:22 +0100

    Fixed add new user

--- confirmation_page.php
+++ confirmation_page.php
@@ -17,14 +17,6 @@ session_start();
 
 require 'dbconnect.php';
 
-$function = $_GET["function"];
-function rand_string( $length ) {
-	$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";	
-	$size = strlen( $chars );
-	for($i = 0; $i < $length; $i++ ) {
-	$str .= $chars[ rand( 0, $size - 1 ) ];}
-	return $str; 
-	}
 
 if ($function == "adduser"){
 
